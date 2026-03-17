25852a7aa04761cf7c9bb1e8741864e47cdcc1b5
Esteban Victorio <steve@Estebans-Mac-Studio.local>
2026-03-17 00:14:33 -0700

    feat: add hero, brief and welcome sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
+
+import { loadEnv } from "vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  integrations: [
+    icon(),
+  ],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
